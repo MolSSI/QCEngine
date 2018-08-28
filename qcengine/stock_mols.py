7399cb4527243dfbb887a3e2990ad063cae9fcb6
Daniel Smith <malorian@me.com>
2018-08-28 13:09:53 -0400

    Mol: Refactors mols and adds hydrogen

--- qcengine/stock_mols.py
+++ qcengine/stock_mols.py
@@ -5,20 +5,26 @@ A small list of molecules used to validate and tests computation.
 import copy
 
 _test_mols = {
-    "water": {
-        "geometry": [
-            0.0, 0.0, -0.1294769411935893, 0.0, -1.494187339479985, 1.0274465079245698, 0.0, 1.494187339479985,
-            1.0274465079245698
-        ],
-        "symbols": ["O", "H", "H"],
-        "connectivity": [[0, 1, 1], [1, 2, 1]]
+    "hydrogen": {
+        "symbols": ["H", "H"],
+        "geometry": [0, 0, -0.65, 0.0, 0.0, 0.65],
+        "molecular_multiplicity": 1,
+        "connectivity": [[0, 1, 1]]
     },
     "lithium": {
         "symbols": ["Li"],
         "geometry": [0, 0, 0],
         "molecular_multiplicity": 2,
         "connectivity": []
-    }
+    },
+    "water": {
+        "geometry": [
+            0.0, 0.0, -0.1294769411935893, 0.0, -1.494187339479985, 1.0274465079245698, 0.0, 1.494187339479985,
+            1.0274465079245698
+        ],
+        "symbols": ["O", "H", "H"],
+        "connectivity": [[0, 1, 1], [0, 2, 1]]
+    },
 }
 
 
