d2ca9d927d7167fb145005033cded9cd99cb92e7
Daniel Smith <malorian@me.com>
2018-10-30 11:54:34 -0400

    Compute: Starts wrapper

--- qcengine/util.py
+++ qcengine/util.py
@@ -2,11 +2,17 @@
 Several import utilities
 """
 
+from contextlib import contextmanager
+
+import traceback
+import time
 import importlib
+import io
 import operator
 
+
 def get_module_function(module, func_name, subpackage=None):
-    """Summary
+    """Obtains a function from a given string
 
     Parameters
     ----------
@@ -34,3 +40,39 @@ def get_module_function(module, func_name, subpackage=None):
     pkg = importlib.import_module(module, subpackage)
 
     return operator.attrgetter(func_name)(pkg)
+
+
+@contextmanager
+def compute_wrapper(capture_output=True):
+    """Wraps compute for timing, output capturing, and raise protection
+    """
+
+    ret = {"stdout": "", "stderr": ""}
+
+    # Start timer
+    comp_time = time.time()
+
+    # Capture stdout/err
+    if capture_output:
+        new_stdout = io.StringIO("Captured stdout:\n\n")
+        new_stderr = io.StringIO("Captured stderr:\n\n")
+
+        old_stdout, sys.stdout = sys.stdout, new_stdout
+        old_stderr, sys.stdout = sys.stdout, new_stderr
+
+    try:
+        yield ret
+        ret["success"] = True
+    except Exception as e:
+        ret["error_message"] = "QCEngine Call Error:\n" + traceback.format_exc()
+        ret["success"] = False
+
+    # Place data
+    ret["wall_time"] = time.time() - comp_time
+    ret["stdout"] = new_stdout.getvalue()
+    ret["stderr"] = new_stderr.getvalue()
+
+    # Replace stdout/err
+    if capture_output:
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
