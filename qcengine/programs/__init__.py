39c0024d0660ebc92e591e0c604fce57b047f071
Justin Turney <justin.turney@gmail.com>
2019-01-16 10:26:35 -0500

    reworked to use register_program

--- qcengine/programs/__init__.py
+++ qcengine/programs/__init__.py
@@ -2,52 +2,28 @@
 Imports the various compute backends
 """
 
-import os
-import re
+programs = {}
 
-programs_path = os.path.dirname(os.path.abspath(__file__))
+def register_program(name, entry_point):
+    if name in programs.keys():
+        raise ValueError('{} is already a registered program.'.format(name))
 
-# Patterns to ignore in the programs directory when looking for programs
-ignore_files = r'^\.|^__init__.py$|^#'
-
-DESCRIPTION = 'description'
-
-programs = []
-for file in os.listdir(programs_path):
-    if file.endswith('.py') and not re.search(ignore_files, file):
-        program = re.sub(r'.py$', '', file)
-        programs.append(program)
-programs.sort()
-
-
-def _get_python_name(name):
-    return name.replace('-', '_')
-
-
-def _attr_setdefault(obj, name, value):
-    """Like dict.setdefault, but for objects."""
-    if not hasattr(obj, name):
-        setattr(obj, name, value)
-    return getattr(obj, name)
-
-
-def get_module(name):
-    """Imports the module for a particular program and returns it."""
-    module_name = '%s.%s' % (__name__, name)
-    module = __import__(module_name,
-                        fromlist=[name, DESCRIPTION],
-                        level=0)
-
-    _attr_setdefault(module, DESCRIPTION, "")
-
-    fn_name = _get_python_name(name)
-    if not hasattr(module, fn_name):
-        raise RuntimeError(
-            "Program module {} ({}) must define function '{}'.".format(module.__name__, module.__file__, fn_name))
-
-    return module
+    programs[name] = { 'entry_point': entry_point }
 
 
 def get_program(name):
-    python_name = _get_python_name(name)
-    return getattr(get_module(python_name), python_name)
+    return programs[name]['entry_point']
+
+
+def get_programs():
+    return programs.keys()
+
+
+from . import psi4
+register_program('psi4', psi4.psi4)
+
+from . import rdkit
+register_program('rdkit', rdkit.rdkit)
+
+from . import torchani
+register_program('torchani', torchani.torchani)
