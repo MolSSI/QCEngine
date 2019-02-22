43059c7f95797880fc84f1d064787659f29b10ef
Sebastian Lee <sjrl423@gmail.com>
2019-02-22 14:27:50 -0800

    Molpro: Minor code cleanup of _format_input()

--- qcengine/programs/molpro.py
+++ qcengine/programs/molpro.py
@@ -16,9 +16,7 @@ def _format_input(input_model, config):
     # Write header info
     input_file.append("!Title")
     memory_mw_core = int(config.memory * (1024 ** 3) / 8e6 / config.ncores)
-
     input_file.append("memory,{},M".format(memory_mw_core))
-
     input_file.append('')
 
     # TODO Hook for global options
@@ -31,25 +29,23 @@ def _format_input(input_model, config):
         input_file.append(s)
     input_file.append('}')
 
-    # write charge and multiplicity
+    # Write charge and multiplicity
     input_file.append('set,charge={}'.format(input_model.molecule.molecular_charge))
     input_file.append('set,multiplicity={}'.format(input_model.molecule.molecular_multiplicity))
-
     input_file.append('')
 
-    # write the basis set
+    # Write the basis set
     input_file.append('basis={')
     input_file.append('default,{}'.format(input_model.model.basis))
     input_file.append('}')
-
     input_file.append('')
 
-    # Write Molpro commands
+    # Start of Molpro Commands
+    # Write energy call
     write_hf = input_model.model.method.lower() in posthf_methods
     if write_hf:
         input_file.append('{hf}')
     input_file.append('{{{:s}}}'.format(input_model.model.method))
-
     input_file.append('')
 
     # Write gradient call if asked for
