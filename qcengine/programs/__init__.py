7005fb8787e7d8f260b8e2f8c7ef1e0e852ed54e
Fang Liu <fangliu.ustc@gmail.com>
2019-02-25 12:32:40 -0500

    TeraChem: removed lines in qcengine/programs/__init__.py about importing terachem stuff.

--- qcengine/programs/__init__.py
+++ qcengine/programs/__init__.py
@@ -9,7 +9,6 @@ from .rdkit import RDKitExecutor
 from .torchani import TorchANIExecutor
 from .molpro import MolproExecutor
 from .dftd3 import DFTD3Executor
-from . import terachem
 
 __all__ = ["register_program", "get_program", "list_all_programs", "list_available_programs"]
 
@@ -60,4 +59,3 @@ register_program(RDKitExecutor())
 register_program(TorchANIExecutor())
 register_program(MolproExecutor())
 register_program(DFTD3Executor())
-register_program('terachem', terachem.terachem)
