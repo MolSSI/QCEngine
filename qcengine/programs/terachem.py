2789e5c2849537ad94a0fb943b150e67e9722019
Fang Liu <fangliu.ustc@gmail.com>
2019-02-25 20:58:08 -0500

    TeraChem: Added output parser and a parse for scratch folder (currently only parsing the charge.xls file in scratch folder)

--- qcengine/programs/terachem.py
+++ qcengine/programs/terachem.py
@@ -6,6 +6,8 @@ from qcelemental.models import ComputeError, FailedOperation, Provenance, Result
 
 from qcengine.units import ureg
 
+import os
+
 def _format_input(input_data, config):
     print("")
     xyz_file = []
@@ -41,3 +43,43 @@ def _format_input(input_data, config):
 def terachem(input_data, config):
     _format_input(input_data, config)
 
+def _output_parse(output_file):
+    output_data=open(output_file).readlines()
+    energy = 0
+    natom = 0
+    gradients = []
+    spin_S2 = -1 # calculated S(S+1)
+    for idx,line in enumerate(output_data):
+        if "FINAL ENERGY" in line:
+            energy = float(line.strip('\n').split()[2])
+        elif "Total atoms" in line:
+            natom = int(line.strip('\n').split()[2])
+        elif "Gradient units are Hartree/Bohr" in line:
+            #Gradient is stored as (dE/dx1,dE/dy1,dE/dz1,dE/dx2,dE/dy2,...)
+            for i in range(idx+3,idx+3+natom):
+               grad = output_data[i].strip('\n').split() 
+               for x in grad:
+                   gradients.append( float(x) )
+        elif "SPIN S-SQUARED" in line:
+            spin_S2 = float(line.strip('\n').split()[2])
+
+    print("Energy "+ str(energy))
+    print("Natom " + str(natom))
+    print("Gradient " + str(gradients))
+    print("S^2: " + str(spin_S2))
+
+def _scr_parse(scrdir):
+    atomic_charge = []
+    atomic_charge_file = os.path.join(scrdir,"charge_mull.xls")
+    if os.path.exists(atomic_charge_file):
+        atomic_charge=_atomic_charge_parse(atomic_charge_file)
+
+    print("\n")
+    print(atomic_charge)
+
+def _atomic_charge_parse(atomic_charge_file):
+    atomic_charge = []
+    data = open(atomic_charge_file).readlines() 
+    for line in data:
+        atomic_charge.append(line.strip('\n').split()[-1])
+    return atomic_charge
