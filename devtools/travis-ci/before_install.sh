2db12984d46db8b760a9d6bf854aed09f2b53ab2
Lori A. Burns <lori.burns@gmail.com>
2019-03-04 16:04:50 -0500

    handle caching in miniconda installer

--- devtools/travis-ci/before_install.sh
+++ devtools/travis-ci/before_install.sh
@@ -2,7 +2,7 @@ set -eu -o pipefail
 
 # Temporarily change directory to $HOME to install software
 pushd .
-cd $HOME
+cd "$HOME"
 
 # Install Miniconda
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
@@ -15,20 +15,39 @@ else
     MINICONDA=Miniconda3-latest-Linux-x86_64.sh
 fi
 MINICONDA_HOME=$HOME/miniconda
-MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget -q https://repo.continuum.io/miniconda/$MINICONDA
-if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
-    echo "Miniconda MD5 mismatch"
-    exit 1
-fi
-bash $MINICONDA -b -p $MINICONDA_HOME
 
-# Configure miniconda
-export PIP_ARGS="-U"
-export PATH=$MINICONDA_HOME/bin:$PATH
+echo "-- Installing latest Miniconda"
+if [ -d "$MINICONDA_HOME/bin" ]; then
+    echo "-- Miniconda latest version FOUND in cache"
     
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
+    # Config settings are not saved in the cache
+    export PIP_ARGS="-U"
+    export PATH=$MINICONDA_HOME/bin:$PATH
+
+    conda config --set always_yes yes --set changeps1 no
+else
+    MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+    echo "-- Miniconda latest version NOT FOUND in cache"
+    wget -q https://repo.continuum.io/miniconda/$MINICONDA
+    if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
+        echo "Miniconda MD5 mismatch"
+        exit 1
+    fi
+    # Travis creates the cached directories for us.
+    # This is problematic when wanting to install Anaconda for the first time...
+    rm -rf "$MINICONDA_HOME"
+    bash $MINICONDA -b -p "$MINICONDA_HOME"
+
+    # Configure miniconda
+    export PIP_ARGS="-U"
+    export PATH=$MINICONDA_HOME/bin:$PATH
+
+    conda config --set always_yes yes --set changeps1 no
+    conda update --q conda
+
+    rm -f $MINICONDA
+fi
+echo "-- Done with latest Miniconda"
 
 # Restore original directory
 popd
