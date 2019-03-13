dbc00246ea8089cfa896cfa76ceba95c3f9cb7d6
Fang Liu <fangliu.ustc@gmail.com>
2019-03-13 11:22:19 -0400

    TeraChem: Added test cases for input builder. Now all tests pass with the updated QCEngineRecords

--- qcengine/programs/tests/test_terachem.py
+++ qcengine/programs/tests/test_terachem.py
@@ -5,7 +5,7 @@ import numpy as np
 import qcelemental as qcel
 from qcelemental.testing import compare_recursive
 
-from qcengine.programs import get_program
+import qcengine as qcng
 
 # qcenginerecords not required, skips whole file
 qcer = pytest.importorskip("qcenginerecords")
@@ -20,8 +20,19 @@ def test_terachem_output_parser(test_case):
      data = terachem_info.get_test_data(test_case)
      inp = qcel.models.ResultInput.parse_raw(data["input.json"])
 
-     output = get_program('terachem').parse_output(data, inp)
+     output = qcng.get_program('terachem').parse_output(data, inp)
 
      output_ref = qcel.models.Result.parse_raw(data["output.json"])
 
      assert compare_recursive(output_ref.dict(), output.dict()) 
+
+@pytest.mark.parametrize('test_case', terachem_info.list_test_cases())
+def test_molpro_input_formatter(test_case):
+
+    # Get output file data
+    data = terachem_info.get_test_data(test_case)
+    inp = qcel.models.ResultInput.parse_raw(data["input.json"])
+
+    # Just test that it runs for now
+    input_file = qcng.get_program('terachem').build_input(inp, qcng.get_config())
+    assert input_file.keys() >= {"commands", "infiles"}
