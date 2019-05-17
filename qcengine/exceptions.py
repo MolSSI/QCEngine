9401276bab85425c33f371585c9129a25c57038e
Daniel Smith <malorian@me.com>
2019-05-17 12:43:15 -0400

    Exc: Header and error type rework

--- qcengine/exceptions.py
+++ qcengine/exceptions.py
@@ -1,11 +1,14 @@
+
+import traceback
+
 from qcelemental.models import ComputeError, FailedOperation
 
-
-class UnknownError(Exception):
+class QCEngineException(Exception):
     """
-    Unknown QCEngine error, the type was not able to be specified.
+    Base QCEngine exception, should never be called explicitly.
     """
-    error_type = "unknown_error"
+    error_type = "base_error"
+    header = "QCEngine Base Error:"
 
     def __init__(self, message, input_data, return_data):
 
@@ -15,38 +18,53 @@ class UnknownError(Exception):
         # Now for your custom code...
         self.input_data = input_data
         self.return_data = return_data
+        self.traceback = traceback.format_exc()
 
-    def format_failed_operation():
+    def format_failed_operation(self):
         rdata = self.return_data.copy()
+
+        message = f"{self.header}\n{str(self)}"
         rdata["error"] = ComputeError(error_type=self.error_type, error_message=self.message)
 
         return FailedOperation(input_data=self.input_data, **rdata)
 
+class UnknownError(QCEngineException):
+    """
+    Unknown QCEngine error, the type was not able to be specified.
+    """
+    error_type = "unknown_error"
+    header = "QCEngine Unknown Error:"
 
-class InputError(QCEngineError):
+
+
+class InputError(QCEngineException):
     """
     Incorrect user parameters, not recoverable. Also may indicate a version issue.
     """
     error_type = "input_error"
+    header = "QCEngine Input Error:"
 
 
-class ResourceError(QCEngineError):
+class ResourceError(QCEngineException):
     """
     Not enough resources for computation such as not enough memory, cores, or disk was not available.
     Recoverable on different compute hardware.
     """
     error_type = "resource_error"
+    header = "QCEngine Resource Error:"
 
 
-class ConvergenceError(QCEngineError):
+class ConvergenceError(QCEngineException):
     """
     Failed iteration convergence error, likely recoverable with tweak parameters.
     """
     error_type = "convergence_error"
+    header = "QCEngine Convergence Error:"
 
 
-class RandomError(QCEngineError):
+class RandomError(QCEngineException):
     """
     Likely recoverable errors such as segmentation faults or disk io problems.
     """
     error_type = "random_error"
+    header = "QCEngine Random Error:"
