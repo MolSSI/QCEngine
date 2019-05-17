cba123eafd03c3f39a8e6ab74a2d7b0c2742a0c1
Daniel Smith <malorian@me.com>
2019-05-17 15:09:37 -0400

    Psi4: Moves wait timeout to 15 seconds for very slow machines

--- qcengine/programs/psi4.py
+++ qcengine/programs/psi4.py
@@ -39,7 +39,7 @@ class Psi4Executor(ProgramExecutor):
         which_prog = which('psi4')
         if which_prog not in self.version_cache:
             with popen([which_prog, '--version']) as exc:
-                exc["proc"].wait(timeout=5)
+                exc["proc"].wait(timeout=15)
             self.version_cache[which_prog] = safe_version(exc["stdout"])
 
         candidate_version = self.version_cache[which_prog]
