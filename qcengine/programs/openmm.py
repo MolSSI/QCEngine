d9da301a731972487fa2149acbea9500477db704
David Dotson <dotsdl@gmail.com>
2019-09-15 12:03:13 -0700

    Converging on desired functionality from OpenMM for QCEngine
    Changes made addressing comments from @jchodera, @dgasmith, and
    @jaimergp:
    * procesing molecule with rdkit -> openforcefield -> openmm
    * limiting scope to `driver`s ('energy', 'gradient')
    * `model` specifies forcefield as `method`; also has `offxml` and `url` fields
    
    Next:
    * need structure positions generated
    * need gradient calculation (`state.getForces()`)
    * need `rdkit` checking in `found` method
    * need `url` handling for forcefield *somewhere* (either in `openforcefield` or here)

--- qcengine/programs/openmm.py
+++ qcengine/programs/openmm.py
@@ -1,5 +1,7 @@
 """
 Calls the OpenMM executable.
+
+Requires RDKit
 """
 import json
 import os
@@ -10,6 +12,7 @@ from qcelemental.models import Result
 from .model import ProgramHarness
 from ..exceptions import InputError, RandomError, ResourceError, UnknownError
 from ..util import execute, popen, temporary_directory
+from ..units import ureg
 
 
 class OpenMMHarness(ProgramHarness):
@@ -21,7 +24,7 @@ class OpenMMHarness(ProgramHarness):
         "scratch": True,
         "thread_safe": False,
         "thread_parallel": True,
-        "node_parallel": False, # don't think openmm can run across multiple nodes
+        "node_parallel": False,
         "managed_memory": True,
     }
 
@@ -30,57 +33,124 @@ class OpenMMHarness(ProgramHarness):
 
     @staticmethod
     def found(raise_error: bool = False) -> bool:
+        # TODO: this harness requires RDKit as well, so this needs checking too
         return which_import('simtk.openmm',
                      return_bool=True,
                      raise_error=raise_error,
                      raise_msg='Please install via `conda install openmm -c omnia`.')
 
+    @staticmethod
+    def _process_molecule_rdkit(jmol):
+        from rdkit import Chem
+        from rdkit.Chem import AllChem
+
+        # Handle errors
+        if abs(jmol.molecular_charge) > 1.e-6:
+            raise InputError("RDKit does not currently support charged molecules.")
+
+        if not jmol.connectivity:  # Check for empty list
+            raise InputError("RDKit requires molecules to have a connectivity graph.")
+
+        # Build out the base molecule
+        base_mol = Chem.Mol()
+        rw_mol = Chem.RWMol(base_mol)
+        for sym in jmol.symbols:
+            rw_mol.AddAtom(Chem.Atom(sym.title()))
+
+        # Add in connectivity
+        bond_types = {1: Chem.BondType.SINGLE, 2: Chem.BondType.DOUBLE, 3: Chem.BondType.TRIPLE}
+        for atom1, atom2, bo in jmol.connectivity:
+            rw_mol.AddBond(atom1, atom2, bond_types[bo])
+
+        mol = rw_mol.GetMol()
+
+        # Write out the conformer
+        natom = len(jmol.symbols)
+        conf = Chem.Conformer(natom)
+        bohr2ang = ureg.conversion_factor("bohr", "angstrom")
+        for line in range(natom):
+            conf.SetAtomPosition(line, (bohr2ang * jmol.geometry[line, 0],
+                                        bohr2ang * jmol.geometry[line, 1],
+                                        bohr2ang * jmol.geometry[line, 2]))  # yapf: disable
+
+        mol.AddConformer(conf)
+        Chem.rdmolops.SanitizeMol(mol)
+
+        return mol
+
     def compute(self, input_data: 'ResultInput', config: 'JobConfig') -> 'Result':
         """
-        Runs OpenMM on given structure, inputs
+        Runs OpenMM on given structure, inputs, in vacuum.
         """
         self.found(raise_error=True)
 
-        from simtk import openmm
-        from simtk.openmm import app
-        from simtk import unit
-        
+        from simtk.openmm.app import Simulation
+        from simtk.openmm import units
 
+        from openforcefield.topology import Molecule
+        from openforcefield.typing.engines.smirnoff import ForceField
+        
         ret_data["success"] = False
 
-        # TODO: SET WORKDIR TO SCRATCH
+        # Set workdir to scratch
         # Location resolution order config.scratch_dir, /tmp
         parent = config.scratch_directory
         with temporary_directory(parent=parent, suffix="_openmm_scratch") as tmpdir:
 
-            # TODO: check what kind of processing we need to do of this input
+            # Grab molecule, forcefield
             jmol = input_data.molecule
+            
+            # TODO: If urls are supported by
+            # `openforcefield.typing.engines.smirnoff.ForceField` already, we
+            # can eliminate the `offxml` and `url` distinction
+            # URL processing can happen there
+            if input_data.model.offxml:
+                offxml = input_data.offxml
+            elif input_data.model.url:
+                # TODO: add pull of offxml data from a url
+                pass
+            else:
+                raise InputError("OpenMM requires either `model.offxml` or `model.url` to be set")
 
-            ff_files = input_data.force_field + input_data.water_model
-            forcefield = app.ForceField(*ff_files)
+            # Process molecule with RDKit
+            rdkit_mol = self._process_molecule_rdkit(jmol)
 
-            # TODO: add nonbondedMethod, nonbondedCutoff as input params
-            system = forcefield.createSystem(jmol.topology,
-                                             nonbondedMethod=app.PME,
-                                             nonbondedCutoff=1*unit.nanometer,
-                                             constraints=input_data.constraints)
+            # Create an OFF Molecule from the RDKit Molecule
+            off_mol = Molecule(rdkit_mol)
 
-            # TODO: continue working here
-            integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+            # Load an OFF Force Field
+            off_forcefield = ForceField(offxml)
 
-            # initialize simulation
-            simulation = Simulation(pdb.topology, system, integrator, platform)
+            # Create OpenMM system in vacuum from forcefield, molecule
+            off_top = off_mol.to_topology()
+            openmm_top = off_top.to_openmm()
+            openmm_system = off_forcefield.create_openmm_system(off_top)
+            
+            # Need an integrator for simulation even if we don't end up using it really
+            integrator = LangevinIntegrator(300*units.kelvin, 1/units.picosecond, 0.002*units.picoseconds)
+
+            # Set platform to CPU explicitly
+            platform = openmm.Platform.getPlatformByName('CPU')
+
+            # Initialize simulation
+            simulation = Simulation(openmm_top, openmm_system, integrator, platform)
+
+            # TODO: need a way to generate positions from the topology
+            # likely an existing tool in the modules we're using since this is
+            # just a molecule floating in space with no solvent
             simulation.context.setPositions(pdb.positions)
-            simulation.minimizeEnergy()
 
-            # set up reporters
-            simulation.reporters.append(PDBReporter('output.pdb', 1000))
-            simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
-                    potentialEnergy=True, temperature=True))
-
-            # execute
+            # Execute driver
             try:
-                simulation.step(input_data.steps)
+                if input_data.driver == "energy":
+                    simulation.context.setPositions(pdb.positions)
+                    state = simulation.context.getState(getEnergy=True)
+                    ret_data["return_result"] = state.getPotentialEnergy()
+                elif input_data.driver == "gradient":
+                    # TODO: calculate forces with OpenMM
+                    pass
+                else:
+                    raise InputError(f"OpenMM can only compute energy and gradient driver methods. Found {input_data.driver}.")
             except:
                 raise
             else:
--- qcengine/programs/tests/test_programs.py
+++ qcengine/programs/tests/test_programs.py
@@ -203,34 +203,12 @@ def test_random_failure_with_success(failure_engine):
 
 @testing.using_openmm
 def test_openmm_task():
-    # might want a structure a bit more involved than a single eneyne for an MD engine
     json_data = copy.deepcopy(_base_json)
-    json_data["molecule"] = qcng.get_molecule("eneyne")
-
-    # what are the required fields?
-    # next review openmm requirements for inputs
-    json_data["platform"] = "CPU"
-    json_data["force_field"] = ["amber14-all.xml"]
-    json_data["water_model"] = ["amber14/tip3pfb.xml"]
-
-    json_data["constraints"] = {}
-    json_data["integrator"] = {"method": "Verlet",
-                               "params": {"timestep": 0.002}}
-
-    json_data["temperature_coupling"] = {"method": "Andersen",
-                                         "params": {"frequency": 1,
-                                                    "temperature": 300}}
-    json_data["pressure_coupling"] = {"method": "MonteCarloAnisotropic",
-                                      "params": {"pressure": [1, 1, 1],
-                                                 "temperature": 300}}
-
-    json_data["energy_minimization"] = {"tolerance" : 5,
-                                        "max_iterations": 1000}
-    json_data["steps"] = 10000
-    json_data["trajectory_output"] = {}
-
-    # consider whether we want to save checkpoints
-
+    json_data["molecule"] = qcng.get_molecule("water")
+    json_data["driver"] = "energy"
+    json_data["model"] = {"method": "smirnoff-1.1.0",
+                          "url": None,
+                          "offxml": "test_forcefields/smirnoff99Frosst.offxml"}
     json_data["keywords"] = {}
 
     ret = qcng.compute(json_data, "openmm", raise_error=True)
