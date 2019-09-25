a06d295db64582138f6b3990f86d05e1551e4411
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-09-25 17:46:20 +0200

    cleaned up tests for turbomole energies

--- qcengine/programs/tests/test_turbomole.py
+++ qcengine/programs/tests/test_turbomole.py
@@ -1,151 +1,42 @@
-import json
-import re
-from subprocess import Popen, PIPE
-
 import qcelemental
+from qcelemental.testing import compare_values
 import qcengine
+from qcengine import testing
 import pytest
 
 
-def test_turbomole():
+@pytest.fixture
+def h2o():
     mol = qcelemental.models.Molecule.from_data("""
             O 0.000000000000     0.000000000000    -0.068516245955
             H 0.000000000000    -0.790689888800     0.543701278274
             H 0.000000000000     0.790689888800     0.543701278274
     """)
+    return mol
 
-    task = {
-            "schema_name": "qcschema_input",
-            "schema_version": 1,
-            "molecule": mol,
-            "driver": "energy",
-            "model" : {"method": "SCF", "basis": "sto-3g"},
-            "keywords": {},
+
+@pytest.mark.parametrize(
+    "method, keywords, ref_energy",
+    [
+        pytest.param('hf', {}, -75.95536954370, marks=testing.using_turbomole),
+        pytest.param('pbe0', {"grid": "m5"}, -76.27371135900, marks=testing.using_turbomole),
+        pytest.param('ricc2', {}, -76.1603807755, marks=testing.using_turbomole),
+        pytest.param('rimp2', {}, -76.1593614075, marks=testing.using_turbomole),
+    ])  # yapf: disable
+def test_turbomole_energies(method, keywords, ref_energy, h2o):
+    resi = {
+        "molecule": h2o,
+        "driver": "energy",
+        "model": {
+            "method": method,
+            "basis": "def2-SVP",
+        },
+        "keywords": keywords,
     }
 
-    # from qcengine.programs.turbomole import TurbomoleHarness
-    # tm = TurbomoleHarness()
-    # v = tm.get_version()
-    # print(v)
-    res = qcengine.compute(task, "turbomole")
-    print(res)
+    res = qcengine.compute(resi, "turbomole", raise_error=True, return_dict=True)
 
+    assert res["driver"] == "energy"
+    assert res["success"] is True
 
-def test_harvest():
-    from qcengine.programs.turbomole.harvester import harvest
-    with open("dscf.out") as handle:
-        text = handle.read()
-    res = harvest(None, text)
-    print(res)
-
-
-def wrap_define():
-    charge = 0
-    mult = 1
-    unrestricted = True
-
-    def occ_num_mo_data(charge, mult, unrestricted=False):
-        """Handles the 'Occupation Number & Molecular Orbital' section
-        of define."""
-        unrestricted = unrestricted or (mult != 1)
-        unpaired = mult - 1
-        charge = int(charge)
-
-        occ_num_mo_data_stdin = f"""eht
-        y
-        {charge}
-        y
-        """
-        if unrestricted:
-            # Somehow Turbomole/define asks us if we want to write
-            # natural orbitals... we don't want to.
-            occ_num_mo_data_stdin = f"""eht
-            y
-            {charge}
-            n
-            u {unpaired}
-            *
-            n
-            """
-        return occ_num_mo_data_stdin
-
-    kwargs = {
-        "charge": 0,
-        "init_guess": occ_num_mo_data_stdin(charge, mult, unrestricted),
-        "title": "QCEngine Turbomole",
-        "xcfunc": "pbe0",
-        "grid": "m4",
-        "scf_conv": 8,
-        "scf_iters": 150,
-    }
-
-    stdin = """
-    {title}
-    a coord
-    *
-    no
-    b
-    all def2-SVP
-    *
-    {init_guess}
-    dft
-    on
-    func
-    {xcfunc}
-    grid
-    {grid}
-
-    scf
-    conv
-    {scf_conv}
-    iter
-    {scf_iters}
-
-    *
-    """.format(**kwargs)
-
-    proc = Popen("define", universal_newlines=True,
-                 stdin=PIPE, stdout=PIPE, stderr=PIPE
-    )
-    stdout, _ = proc.communicate(stdin)#, timeout=3)
-    proc.terminate()
-
-# @pytest.mark.parametrize(
-    # 'program,basis,keywords',
-    # [
-        # pytest.param('turbomole', 'aug-pvdz', {}),
-    # ])  # yapf: disable
-# def test_sp_hf_rhf(program, basis, keywords):
-    # mol = qcelemental.models.Molecule.from_data("""
-            # O 0.000000000000     0.000000000000    -0.068516245955
-            # H 0.000000000000    -0.790689888800     0.543701278274
-            # H 0.000000000000     0.790689888800     0.543701278274
-    # """)
-
-    # resi = {
-        # "molecule": mol,
-        # "driver": "energy",
-        # "model": {
-            # "method": "hf",
-            # "basis": basis
-        # },
-        # "keywords": keywords,
-    # }
-
-    # res = qcengine.compute(resi, program, raise_error=True, return_dict=True)
-
-    # assert res["driver"] == "energy"
-    # assert "provenance" in res
-    # assert res["success"] is True
-
-    # # aug-cc-pvdz
-    # scf_tot = -76.0413815332
-
-    # atol = 1.e-6
-    # assert compare_values(scf_tot, res["return_result"], atol=atol)
-
-
-if __name__ == "__main__":
-    test_turbomole()
-    # test_harvest()
-    # wrap_define()
+    assert compare_values(ref_energy, res["return_result"])
