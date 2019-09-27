dcbdd13e2ad3d77922b9e71198283f70669a8330
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-09-27 11:50:24 +0200

    moved methods and keywords to dict

--- qcengine/programs/turbomole/methods.py
+++ qcengine/programs/turbomole/methods.py
@@ -1,2 +1,15 @@
+# Available methods
+METHODS = {
+    "ricc2": "rimp2 rimp3 rimp4 ricc2".split(),
+    # Set some appropriate predefined functionals that can be directly
+    # substituted into the control file.
+    # "dft": ""
+}
 
-ricc2_methods = "rimp2 rimp3 rimp4 ricc2 riccsd riccsd(t)".split()
+# Available keywords
+KEYWORDS = {
+    # Resolution of identity
+    "ri": "rijk ri marij".split(),
+    # Dispersion correction
+    "dsp": "d3 d3bj".split(),
+}
