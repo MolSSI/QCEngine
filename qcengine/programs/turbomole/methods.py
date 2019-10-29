d0ddc485e82f54e7be21b86c692f1eebec224e4f
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-10-29 08:23:45 +0100

    switch from "...".spli() to explicit lists in available methods

--- qcengine/programs/turbomole/methods.py
+++ qcengine/programs/turbomole/methods.py
@@ -1,6 +1,6 @@
 # Available methods
 METHODS = {
-    "ricc2": "rimp2 rimp3 rimp4 ricc2".split(),
+    "ricc2": ["rimp2", "rimp3", "rimp4", "ricc2"],
     # Set some appropriate predefined functionals that can be directly
     # substituted into the control file.
     # "dft": ""
@@ -9,7 +9,7 @@ METHODS = {
 # Available keywords
 KEYWORDS = {
     # Resolution of identity
-    "ri": "rijk ri marij".split(),
+    "ri": ["rijk", "ri", "marij"],
     # Dispersion correction
-    "dsp": "d3 d3bj".split(),
+    "dsp": ["d3", "d3bj"],
 }
