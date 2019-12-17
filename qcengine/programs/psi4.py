8936a28ae256be0deaf98a88f28470e6663bac6b
Lori A. Burns <lori.burns@gmail.com>
2019-12-16 19:26:53 -0500

    extras

--- qcengine/programs/psi4.py
+++ qcengine/programs/psi4.py
@@ -152,7 +152,7 @@ class Psi4Harness(ProgramHarness):
                     psi4.set_memory(f"{config.memory}GB", quiet=True)
                     psi4.core.IOManager.shared_object().set_default_path(str(tmpdir))
                     output_data = psi4.schema_wrapper.run_qcschema(input_model).dict()
-                    print(output_data)
+                    print(output_data["extras"])
                     output_data["extras"]["psiapi_evaluated"] = True
                     success = True
                     psi4.core.IOManager.shared_object().set_default_path(orig_scr)
