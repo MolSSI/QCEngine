d487de1a52bf7500ccc6a73292bbba8a9aebe932
Lori A. Burns <lori.burns@gmail.com>
2019-12-17 00:42:20 -0500

    when atexit not appropriate

--- qcengine/programs/psi4.py
+++ qcengine/programs/psi4.py
@@ -151,7 +151,7 @@ class Psi4Harness(ProgramHarness):
                     psi4.core.set_num_threads(config.ncores, quiet=True)
                     psi4.set_memory(f"{config.memory}GB", quiet=True)
                     psi4.core.IOManager.shared_object().set_default_path(str(tmpdir))
-                    output_data = psi4.schema_wrapper.run_qcschema(input_model).dict()
+                    output_data = psi4.schema_wrapper.run_qcschema(input_model, postclean=False).dict()
                     print(output_data["extras"])
                     output_data["extras"]["psiapi_evaluated"] = True
                     success = True
