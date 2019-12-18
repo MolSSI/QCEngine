f6259e1914f3e77fe004417cce88fa38b666f326
Sebastian Lee <sjrl423@gmail.com>
2019-12-17 16:58:57 -0800

    entos: Updated QCER hash and program_overview.rst

--- qcengine/testing.py
+++ qcengine/testing.py
@@ -12,7 +12,7 @@ from qcelemental.util import which, which_import
 
 import qcengine as qcng
 
-QCENGINE_RECORDS_COMMIT = "ee405ca"
+QCENGINE_RECORDS_COMMIT = "aa4a1b2"
 
 
 def _check_qcenginerecords(return_data=False):
