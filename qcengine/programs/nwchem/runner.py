436d62716942cb44dd20d8ab6ecbf440498c67df
ahurta92 <ahurta92@gmail.com>
2020-01-23 12:57:44 -0500

    avoid keyerror when driver==properties

--- qcengine/programs/nwchem/runner.py
+++ qcengine/programs/nwchem/runner.py
@@ -238,7 +238,11 @@ task python
             qcvars["CURRENT HESSIAN"] = nwhess
 
         # Normalize the output as a float or list of floats
-        retres = qcvars[f"CURRENT {input_model.driver.upper()}"]
+        if (input_model.driver.upper()=="PROPERTIES"):
+            retres = qcvars[f"CURRENT ENERGY" ]
+        else:
+            retres = qcvars[f"CURRENT {input_model.driver.upper()}"]
+
         if isinstance(retres, Decimal):
             retres = float(retres)
         elif isinstance(retres, np.ndarray):
