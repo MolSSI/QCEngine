7423220965dd2f919033c4b051742e630765b1fc
Lori A. Burns <lori.burns@gmail.com>
2020-02-04 18:59:01 -0500

    make psi4 harness safe for psiapi file cleaning

--- qcengine/testing.py
+++ qcengine/testing.py
@@ -153,7 +153,7 @@ _programs = {
     "mp2d": which("mp2d", return_bool=True),
     "nwchem": which("nwchem", return_bool=True),
     "psi4": is_program_new_enough("psi4", "1.2"),
-    "psi4_14": is_program_new_enough("psi4", "1.4a2.dev500"),
+    "psi4_runqcsk": is_program_new_enough("psi4", "1.4a2.dev160"),
     "qcdb": which_import("qcdb", return_bool=True),
     "qchem": is_program_new_enough("qchem", "5.2"),
     "rdkit": which_import("rdkit", return_bool=True),
