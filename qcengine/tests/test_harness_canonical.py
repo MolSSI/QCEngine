892b5025279f2cb8abd3491f73dd3e0f1150122c
Muammar El Khatib <muammarelkhatib@gmail.com>
2020-02-07 09:01:02 -0800

    Added Orca canonical example.

--- qcengine/tests/test_harness_canonical.py
+++ qcengine/tests/test_harness_canonical.py
@@ -18,6 +18,7 @@ _canonical_methods = [
     ("mp2d", {"method": "MP2-DMP2"}),
     ("nwchem", {"method": "hf", "basis": "6-31G"}),
     ("openmm", {"method": "openff-1.0.0", "basis": "smirnoff"}),
+    ("orca", {"method": "hf", "basis": "6-31G"}),
     ("psi4", {"method": "hf", "basis": "6-31G"}),
     ("qchem", {"method": "hf", "basis": "6-31G"}),
     ("rdkit", {"method": "UFF"}),
