98e0f4c23019e461cdc3dd7fbd2db2b0dba5dbd7
ahurta92 <ahurta92@gmail.com>
2020-04-27 17:32:57 -0400

    wrote compute, fix mol.keywords, wrote parse_output

--- qcengine/programs/madness/runner.py
+++ qcengine/programs/madness/runner.py
@@ -20,9 +20,9 @@ from ...exceptions import InputError
 from ...util import execute, create_mpi_invocation
 from ..model import ProgramHarness
 
-# from .germinate import muster_modelchem
-# from .harvester import extract_formatted_properties, harvest
-# from .keywords import format_keywords
+from .germinate import muster_modelchem
+from .harvester import extract_formatted_properties, harvest
+from .keywords import format_keywords
 
 pp = pprint.PrettyPrinter(width=120, compact=True, indent=1)
 logger = logging.getLogger(__name__)
@@ -115,27 +115,27 @@ class MadnessHarness(ProgramHarness):
         return self.version_cache[which_prog]
 
 
-    def compute(self, input_model: "AtomicInput", config: "TaskConfig") -> "AtomicResult": pass
-#          """
-#          Runs madness in executable mode
-#          """
-#          self.found(raise_error=True)
+    def compute(self, input_model: "AtomicInput", config: "TaskConfig") -> "AtomicResult": 
+        """
+         Runs madness in executable mode
+         """
+        self.found(raise_error=True)
 
-#          job_inputs = self.build_input(input_model, config)
-#          success, dexe = self.execute(job_inputs)
+        job_inputs = self.build_input(input_model, config)
+        success, dexe = self.execute(job_inputs)
 
-#          if "There is an error in the input file" in dexe["stdout"]:
-#              raise InputError(dexe["stdout"])
-#          if "not compiled" in dexe["stdout"]:
-#              # recoverable with a different compilation with optional modules
-#              raise InputError(dexe["stdout"])
+        if "There is an error in the input file" in dexe["stdout"]:
+             raise InputError(dexe["stdout"])
+        if "not compiled" in dexe["stdout"]:
+             # recoverable with a different compilation with optional modules
+            raise InputError(dexe["stdout"])
 
-#          if success:
-#              dexe["outfiles"]["stdout"] = dexe["stdout"]
-#              dexe["outfiles"]["stderr"] = dexe["stderr"]
-#              return self.parse_output(dexe["outfiles"], input_model)
-#          else:
-#              raise UnknownError(dexe["stderr"])
+        if success:
+            dexe["outfiles"]["stdout"] = dexe["stdout"]
+            dexe["outfiles"]["stderr"] = dexe["stderr"]
+            return self.parse_output(dexe["outfiles"], input_model)
+        else:
+            raise UnknownError(dexe["stderr"])
 
     def build_input(
          self, input_model: AtomicInput, config: TaskConfig, template: Optional[str] = None
@@ -146,17 +146,13 @@ class MadnessHarness(ProgramHarness):
         opts=copy.deepcopy(input_model.keywords)
         opts = {k.lower(): v for k, v in opts.items()}
     
-        # Handle memory (ROBERT)  Look in keywords as well
-        # for nwchem, [GiB] --> [B]
-        # someday, replace with this: opts['memory'] = str(int(config.memory * (1024**3) / 1e6)) + ' mb'
-        #memory_size = int(config.memory * (1024 ** 3))
-        #if config.use_mpiexec:  # It is the memory per MPI rank
-        #    memory_size //= config.nnodes * config.ncores // config.cores_per_rank
-        #opts["memory"] = memory_size
 
         # Handle Molecule
         molcmd, moldata = input_model.molecule.to_string(dtype="madness", units="bohr", return_data=True)
-        opts.update(moldata["keywords"])
+        molData={}
+        for k,v in moldata["keywords"].items():
+            molData['dft__'+k]=v
+        opts.update(molData)
         
         ## Handle Calc Type (ROBERT)
         mdccmd, mdcopts = muster_modelchem(input_model.model.method, input_model.driver, opts.pop("qc_module", False))
@@ -178,7 +174,7 @@ class MadnessHarness(ProgramHarness):
         ## Determine the command
            # Determine the command
         madnessrec["command"] = [which("madness")]
-
+        print(madnessrec["infiles"]["input"])
         return madnessrec
 
 
@@ -193,50 +189,52 @@ class MadnessHarness(ProgramHarness):
 
     def parse_output(
         self, outfiles: Dict[str, str], input_model: "AtomicInput"
-    ) -> "AtomicResult": pass # lgtm: [py/similar-function]
+    ) -> "AtomicResult":  # lgtm: [py/similar-function]
 
-#         # Get the stdout from the calculation (required)
-#         stdout = outfiles.pop("stdout")
 
-#         # Read the NWChem stdout file and, if needed, the hess or grad files
-#         qcvars, nwhess, nwgrad, nwmol, version, errorTMP = harvest(input_model.molecule, stdout, **outfiles)
+       # Get the stdout from the calculation (required)
+        stdout = outfiles.pop("stdout")
 
-#         if nwgrad is not None:
-#             qcvars["CURRENT GRADIENT"] = nwgrad
+        # Read the NWChem stdout file and, if needed, the hess or grad files
+        qcvars, nwhess, nwgrad, nwmol, version, errorTMP = harvest(input_model.molecule, stdout, **outfiles)
 
-#         if nwhess is not None:
-#             qcvars["CURRENT HESSIAN"] = nwhess
+        if nwgrad is not None:
+           qcvars["CURRENT GRADIENT"] = nwgrad
 
-#         # Normalize the output as a float or list of floats
-#         if input_model.driver.upper() == "PROPERTIES":
-#             retres = qcvars[f"CURRENT ENERGY"]
-#         else:
-#             retres = qcvars[f"CURRENT {input_model.driver.upper()}"]
+        if nwhess is not None:
+           qcvars["CURRENT HESSIAN"] = nwhess
 
-#         if isinstance(retres, Decimal):
-#             retres = float(retres)
-#         elif isinstance(retres, np.ndarray):
-#             retres = retres.tolist()
+        # Normalize the output as a float or list of floats
+        if input_model.driver.upper() == "PROPERTIES":
+           retres = qcvars[f"CURRENT ENERGY"]
+        else:
+           print(qcvars)
+           retres = qcvars[f"CURRENT {input_model.driver.upper()}"]
 
-#         # Get the formatted properties
-#         qcprops = extract_formatted_properties(qcvars)
+        if isinstance(retres, Decimal):
+           retres = float(retres)
+        elif isinstance(retres, np.ndarray):
+           retres = retres.tolist()
 
-#         # Format them inout an output
-#         output_data = {
-#             "schema_name": "qcschema_output",
-#             "schema_version": 1,
-#             "extras": {"outfiles": outfiles, **input_model.extras},
-#             "properties": qcprops,
-#             "provenance": Provenance(creator="NWChem", version=self.get_version(), routine="nwchem"),
-#             "return_result": retres,
-#             "stdout": stdout,
-#         }
+      # Get the formatted properties
+        qcprops = extract_formatted_properties(qcvars)
 
-#         # got to even out who needs plump/flat/Decimal/float/ndarray/list
-#         # Decimal --> str preserves precision
-#         output_data["extras"]["qcvars"] = {
-#             k.upper(): str(v) if isinstance(v, Decimal) else v for k, v in qcel.util.unnp(qcvars, flat=True).items()
-#         }
+      # Format them inout an output
+        output_data = {
+           "schema_name": "qcschema_output",
+           "schema_version": 1,
+           "extras": {"outfiles": outfiles, **input_model.extras},
+           "properties": qcprops,
+           "provenance": Provenance(creator="MADNESS", version=self.get_version(), routine="madness"),
+           "return_result": retres,
+           "stdout": stdout,
+        }
 
-#         output_data["success"] = True
-#         return AtomicResult(**{**input_model.dict(), **output_data})
+      # got to even out who needs plump/flat/Decimal/float/ndarray/list
+      # Decimal --> str preserves precision
+        output_data["extras"]["qcvars"] = {
+           k.upper(): str(v) if isinstance(v, Decimal) else v for k, v in qcel.util.unnp(qcvars, flat=True).items()
+        }
+
+        output_data["success"] = True
+        return AtomicResult(**{**input_model.dict(), **output_data})
