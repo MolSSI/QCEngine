38aa76250560ebb7a61e754d96bed9856d6fb591
ahurta92 <ahurta92@gmail.com>
2020-05-04 12:00:02 -0400

    added k=7 to keywords of test.  k=7 is a default but its okay to define

--- qcengine/programs/tests/test_standard_suite_hf.py
+++ qcengine/programs/tests/test_standard_suite_hf.py
@@ -158,7 +158,7 @@ def test_sp_hf_rohf(program, basis, keywords, nh2):
 
 
 @pytest.mark.parametrize(
-    "program,basis,keywords", [pytest.param("madness", None, {"dft__aobasis": "sto-3g", "dft__econv": 1.0000e-05}),],
+    "program,basis,keywords", [pytest.param("madness", None, {"dft__k":7,"dft__aobasis": "sto-3g", "dft__econv": 1.0000e-05}),],
 )
 def test_mad_hf(program, basis, keywords, h2o):
     resi = {"molecule": h2o, "driver": "energy", "model": {"method": "hf", "basis": basis}, "keywords": keywords}
