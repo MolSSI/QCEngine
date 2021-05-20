bce5c80ac4fb39ecd4c05c667a3736b73667161e
Colton Hicks <github@coltonhicks.com>
2021-05-20 15:19:43 -0700

    Add test

--- qcengine/tests/test_procedures.py
+++ qcengine/tests/test_procedures.py
@@ -3,7 +3,7 @@ Tests the DQM compute dispatch module
 """
 
 import pytest
-from qcelemental.models import DriverEnum, OptimizationInput
+from qcelemental.models import DriverEnum, OptimizationInput, FailedOperation
 from qcelemental.models.common_models import Model
 from qcelemental.models.procedures import OptimizationSpecification, QCInputSpecification, TDKeywords, TorsionDriveInput
 
@@ -98,6 +98,23 @@ def test_berny_stdout(input_data):
     ret = qcng.compute_procedure(input_data, "berny", raise_error=True)
     assert ret.success is True
     assert "All criteria matched" in ret.stdout
+
+
+@using("psi4")
+@using("berny")
+def test_berny_failed_gradient_computation(input_data):
+
+    input_data["initial_molecule"] = qcng.get_molecule("water")
+    input_data["input_specification"]["model"] = {"method": "HF", "basis": "sto-3g"}
+    input_data["input_specification"]["keywords"] = {"badpsi4key", "badpsi4value"}
+    input_data["keywords"]["program"] = "psi4"
+
+    input_data = OptimizationInput(**input_data)
+
+    ret = qcng.compute_procedure(input_data, "berny", raise_error=False)
+    assert isinstance(ret, FailedOperation)
+    assert ret.success is False
+    assert ret.error.error_type == qcng.exceptions.InputError.error_type
 
 
 @using("geometric")
