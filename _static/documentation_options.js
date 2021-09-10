98ca1c9c850be964a6a8491f458a9dd6fbf54ffb
dotsdl <dotsdl@users.noreply.github.com>
2021-09-10 22:12:25 +0000

    Deploying to gh-pages from @ MolSSI/QCEngine@6e6daf00e3dd0e9546dbb1eb228d4c3b374535b4 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g3939a5b',
+    VERSION: '0+untagged.1.g6e6daf0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
