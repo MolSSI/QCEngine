19cfce728d3a9115fec1f64f90752e6d0d502a68
Lori A. Burns <lori.burns@gmail.com>
2021-09-21 15:21:24 -0400

    try to fix nwc ci 2

--- qcengine/programs/nwchem/keywords.py
+++ qcengine/programs/nwchem/keywords.py
@@ -13,7 +13,7 @@ def format_keyword(keyword: str, val: Any, lop_off: bool = True) -> Tuple[str, s
 
     # complete hack
     if keyword.upper() == "MEMORY":
-        return keyword.lower(), f"{val} byte"
+        return keyword.lower(), f"{val} units byte"
 
     elif isinstance(val, list):
         text = " ".join([str(v) for v in val])
