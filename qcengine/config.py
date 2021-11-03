d30a7354683a4eae07cf51959c499968e37507e9
Ke <justxwy@126.com>
2021-11-03 11:06:15 +0800

    Update config.py
    delete device config

--- qcengine/config.py
+++ qcengine/config.py
@@ -8,7 +8,6 @@ import logging
 import os
 import socket
 from typing import Any, Dict, Optional, Union
-from typing_extensions import Literal
 
 import pydantic
 
@@ -159,8 +158,7 @@ class TaskConfig(pydantic.BaseModel):
     scratch_messy: bool = pydantic.Field(
         False, description="Leave scratch directory and contents on disk after completion."
     )
-    device: Literal['cpu', 'cuda'] = 'cpu' # which device to perform calculation
-    
+        
     class Config:
         extra = "forbid"
 
