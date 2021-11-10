7913f19db52220a329941e783bb22de28e093ddc
WardLT <WardLT@users.noreply.github.com>
2021-11-10 15:21:38 +0000

    Deploying to gh-pages from @ MolSSI/QCEngine@4a92ee6a0dcea42316e00f9f9a7c07d95cbda111 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g0612e81',
+    VERSION: '0+untagged.1.g4a92ee6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
