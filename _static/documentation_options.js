8ed40f7dc2ca424f6731b13a217174cd0cca87c2
dotsdl <dotsdl@gmail.com>
2022-01-19 00:31:50 +0000

    Deploying to gh-pages from @ MolSSI/QCEngine@43b07131d7586eae9c9467fcc091a573758a0187 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gcca9316',
+    VERSION: '0+untagged.1.g43b0713',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
