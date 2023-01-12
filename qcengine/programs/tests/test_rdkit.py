e47748a3456abfe29aae19ce521c12bb19e5719a
1eyedt0ad <mmars@vt.edu>
2023-01-12 16:51:53 -0500

    added testing module for RDKit

---
+++ qcengine/programs/tests/test_rdkit.py
@@ -0,0 +1,8 @@
+"""Tests for RDKit functionality"""
+import numpy as np
+import pytest
+import qcelemental as qcel
+from qcelemental.testing import compare_values
+
+import qcengine as qcng
+from qcengine.testing import using
