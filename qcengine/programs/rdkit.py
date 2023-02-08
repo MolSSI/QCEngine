c9680cc57af76a0d60440abe1d2a489965a87063
dr-marsmm <mmars@vt.edu>
2023-02-08 15:42:43 -0500

    no 1.5 bonds in stock_mols, no stereochemistry returns None

--- qcengine/programs/rdkit.py
+++ qcengine/programs/rdkit.py
@@ -272,6 +272,9 @@ class DescriptorConstructor:
         else:
             stereochem = Chem.FindMolChiralCenters(mol)
 
+        if stereochem == []:
+            stereochem = None
+
         return stereochem
 
     @property
