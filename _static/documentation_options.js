022da2f3e71efc42a7f0b2ffc480491c4b9e2b6a
loriab <lori.burns@gmail.com>
2023-08-08 00:06:11 +0000

    Deploying to gh-pages from @ MolSSI/QCEngine@e5a43ba1430a013b12b163447a53a084dfe90613 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gd18cfda',
+    VERSION: '0+untagged.1.ge5a43ba',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
