96418579d1f70ea057ddab8a022f3d3673649937
Lori A. Burns <lori.burns@gmail.com>
2023-11-01 10:40:21 -0400

    more efmo

--- qcengine/programs/gamess/keywords.py
+++ qcengine/programs/gamess/keywords.py
@@ -37,11 +37,19 @@ def format_keywords(keywords: Dict[str, Any]) -> str:
 
     grouped_lines = {}
     for group, opts in sorted(grouped_options.items()):
+        is_block = False
         line = []
         line.append(f"${group.lower()}")
         for key, val in sorted(grouped_options[group].items()):
-            line.append("=".join(format_keyword(key, val, lop_off=False)))
+            if key == "":
+                is_block = True
+                line.extend(val.split("\n"))
+            else:
+                line.append("=".join(format_keyword(key, val, lop_off=False)))
         line.append("$end\n")
-        grouped_lines[group] = textwrap.fill(" ".join(line), initial_indent=" ", subsequent_indent="  ")
+        if is_block:
+            grouped_lines[group] = "\n".join(" " + ln for ln in line)
+        else:
+            grouped_lines[group] = textwrap.fill(" ".join(line), initial_indent=" ", subsequent_indent="  ")
 
     return "\n".join(grouped_lines.values()) + "\n"
