e8165721a6352bbb7aac50808606cc91e6b6c48f
Lori A. Burns <lori.burns@gmail.com>
2025-01-14 15:45:10 -0500

    modern openmm

--- qcengine/programs/tests/test_dftd3_mp2d.py
+++ qcengine/programs/tests/test_dftd3_mp2d.py
@@ -44,7 +44,7 @@ def test_dftd3_error():
         input_data["driver"] = "properties"
         ret = qcng.compute(input_data, "dftd3", raise_error=True)
 
-    assert "Driver properties not implemented" in str(exc.value)
+    assert "properties not implemented" in str(exc.value)
 
     # Test extension
     with pytest.raises(qcng.exceptions.InputError) as exc:
