706740965f470d7edb40b5c8d722a5c237accab7
Lori Burns <lori.burns@gmail.com>
2026-02-14 23:05:13 -0500

    fix address

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -212,8 +212,8 @@ intersphinx_mapping = {'python': ('https://docs.python.org/3.10', None),
                        'scipy': ('https://docs.scipy.org/doc/scipy/', None),
                        'matplotlib': ('https://matplotlib.org/stable/', None),
                        "qcelemental": ("https://molssi.github.io/QCElemental/dev/", None),
-                       "qcportal": ("http://docs.qcarchive.molssi.org/projects/QCPortal/en/latest/", None),
-                       "qcfractal": ("http://docs.qcarchive.molssi.org/projects/QCFractal/en/latest/", None),
+                       "qcengine": ("https://molssi.github.io/QCEngine/", None),
+                       "qcfractal": ("https://molssi.github.io/QCFractal/", None),
                       }
 
 # -- Options for todo extension ----------------------------------------------
