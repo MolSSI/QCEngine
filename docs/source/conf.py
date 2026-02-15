2f1715c328e52ea93282549efb8561c2073c8fbf
Lori Burns <lori.burns@gmail.com>
2026-02-14 22:51:02 -0500

    fix address

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -211,7 +211,7 @@ intersphinx_mapping = {'python': ('https://docs.python.org/3.10', None),
                        "numpy": ("https://numpy.org/doc/stable/", None),
                        'scipy': ('https://docs.scipy.org/doc/scipy/', None),
                        'matplotlib': ('https://matplotlib.org/stable/', None),
-                       "qcelemental": ("http://docs.qcarchive.molssi.org/projects/QCElemental/en/latest/", None),
+                       "qcelemental": ("https://molssi.github.io/QCElemental/dev/", None),
                        "qcportal": ("http://docs.qcarchive.molssi.org/projects/QCPortal/en/latest/", None),
                        "qcfractal": ("http://docs.qcarchive.molssi.org/projects/QCFractal/en/latest/", None),
                       }
