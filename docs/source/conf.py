9eb3157de388100a396faccb205f9a2bde376a8a
Lori A. Burns <lori.burns@gmail.com>
2026-08-14 21:18:21 -0400

    Fix pyberny 0.7 CI compatibility (#516)
    * Fix pyberny 0.7 CI compatibility
    
    * changelog
    
    * Change release date for v0.50.1 to unreleased
    
    Update changelog for unreleased version v0.50.1.
    
    * fix ci for libxc 7.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -170,7 +170,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'QCEngine.tex', 'QCEngine Documentation', 'The QCArchive Development Team', 'manual'),
+    (master_doc, "QCEngine.tex", "QCEngine Documentation", "The QCArchive Development Team", "manual"),
 ]
 
 
@@ -178,7 +178,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, 'qcengine', 'QCEngine Documentation', [author], 1)]
+man_pages = [(master_doc, "qcengine", "QCEngine Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------------
